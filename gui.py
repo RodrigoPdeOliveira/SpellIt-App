34547489ce68296e41c3d4b8cceb48f03788488a
Rodrigo Oliveira <rodrikpoliveira@gmail.com>
2023-03-30 14:34:27 -0300

    Added skip button

--- gui.py
+++ gui.py
@@ -1,10 +1,12 @@
 import PySimpleGUI as sg
+from main import WordChooser
 
+wc = WordChooser()
 sg.theme("DarkBlue12")
 layout = [
     [sg.Text("Spell It!")],
     [sg.InputText(key="textInput", expand_x=True, justification="center", font="Arial 35 bold")],
-    [sg.Button("Play", expand_x=True), sg.Button("Hint", expand_x=True)],
+    [sg.Button("Play", expand_x=True), sg.Button("Hint", expand_x=True), sg.Button("Skip", expand_x=True)],
     [sg.Button("Enter", expand_x=True, size=(None, 20))],
 ]
 
@@ -21,8 +23,12 @@ while True:
     event, values = window.read()
     if event == sg.WIN_CLOSED:
         break
+
     if event == "Enter" or event == "Return:36":
         text = values["textInput"]
         window["textInput"].update("")
+
+    if event == "Play":
+        wc.say_word()
 
 window.close()
