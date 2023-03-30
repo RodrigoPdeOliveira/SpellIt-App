fc969c03f1c8f12b55d668aed2690aacf984891a
Rodrigo Oliveira <rodrikpoliveira@gmail.com>
2023-03-30 11:12:36 -0300

    Optimized word randomizer logic

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
-from random import choice
+from random import randint
 
 
 class WordChooser:
     def __init__(self) -> None:
         with open("./english_words.csv") as f:
-            self.csv_file = f.read().split(",")
-        self.random_word = choice(self.csv_file)
+            self.words = set(word.strip() for word in f.read().split(","))
+        self.random_word = self.choose_random_word()
+
+    def choose_random_word(self):
+        return self.words[randint(0, len(self.words) - 1)]
 
     def randomize_word(self):
         current_word = self.random_word
-        new_word = choice(self.csv_file)
+        new_word = self.choose_random_word()
 
         if new_word != current_word:
             self.random_word = new_word
             return new_word
         else:
-            self.random_word = self.randomize_word()
+            return self.randomize_word()
