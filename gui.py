1296376ed66dba776ccbdeb8fae3669c51d6c34b
Rodrigo Oliveira <rodrikpoliveira@gmail.com>
2023-03-30 11:01:48 -0300

    First iteration of GUI

---
+++ gui.py
@@ -0,0 +1,28 @@
+import PySimpleGUI as sg
+
+sg.theme("DarkBlue12")
+layout = [
+    [sg.Text("Spell It!")],
+    [sg.InputText(key="textInput", expand_x=True, justification="center", font="Arial 35 bold")],
+    [sg.Button("Play", expand_x=True), sg.Button("Hint", expand_x=True)],
+    [sg.Button("Enter", expand_x=True, size=(None, 20))],
+]
+
+window = sg.Window(
+    "SpellIt",
+    layout,
+    size=(700, 400),
+    font="Helvetica 20",
+    element_justification="center",
+    return_keyboard_events=True,
+)
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    if event == "Enter" or event == "Return:36":
+        text = values["textInput"]
+        window["textInput"].update("")
+
+window.close()
