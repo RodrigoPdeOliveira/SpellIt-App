18c617bdb4ffd3a89d31fac472c752a1d4554b86
Rodrigo Oliveira <rodrikpoliveira@gmail.com>
2023-04-13 12:53:20 -0300

    Added points tally

--- gui.py
+++ gui.py
@@ -1,12 +1,21 @@
 import PySimpleGUI as sg
 from main import WordChooser
 
+
+def update_success_rate():
+    return 0 if n_of_tentatives == 0 else (correct_guesses / n_of_tentatives) * 100
+
+
+correct_guesses = 0
+n_of_tentatives = 0
+percent_of_success = update_success_rate()
+
 wc = WordChooser()
 sg.theme("DarkBlue12")
 layout = [
     [sg.Text("Spell It!")],
     [sg.InputText(key="textInput", expand_x=True, justification="center", font="Arial 35 bold")],
-    [sg.Text("Correct: "), sg.Text("Nº of words tried: "), sg.Text("% of success: ")],
+    [sg.Text("Correct: 0", key="correct"), sg.Text("Nº of words tried: 0", key="attempts"), sg.Text("% of success: 0%", key="success_rate")],
     [sg.Button("Play", expand_x=True)],
     [sg.Button("Hint", expand_x=True), sg.Button("Skip", expand_x=True)],
     [sg.Button("Enter", expand_x=True)],
@@ -29,6 +38,20 @@ while True:
     if event == "Enter" or event == "Return:36":
         text = values["textInput"].lower()
         window["textInput"].update("")
+        n_of_tentatives += 1
+
+        if text == wc.random_word:
+            correct_guesses += 1
+            wc.randomize_word()
+            wc.set_word()
+        else:
+            print("WRONG")
+
+        percent_of_success = update_success_rate()
+
+        window["correct"].update(f"Correct: {correct_guesses}")
+        window["attempts"].update(f"Nº of words tried: {n_of_tentatives}")
+        window["success_rate"].update(f"% of success: {percent_of_success:.2f}%")
 
     if event == "Play":
         wc.say_word()
